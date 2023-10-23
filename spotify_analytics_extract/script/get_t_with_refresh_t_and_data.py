7821e2e5cdec4ff38bfa6d4d4e550a17aa4fd4f4
ah-portfolio <110063004+ah-portfolio@users.noreply.github.com>
2023-10-23 10:58:24 +0200

    Update get_t_with_refresh_t_and_data.py

--- spotify_analytics_extract/script/get_t_with_refresh_t_and_data.py
+++ spotify_analytics_extract/script/get_t_with_refresh_t_and_data.py
@@ -80,83 +80,10 @@ for i in range (50):
     ])
 
 df = pd.DataFrame(data,columns=headers)
-print(df)
+#print(df)
 
 
 client = storage.Client()
 bucket = client.get_bucket('adil_bucket_test_asa')
     
 bucket.blob(f'raw_spotify/test_{datetime.today()}.csv').upload_from_string(df.to_csv(index=False,sep=';'), 'text/csv')
-
-
-# # current date and time plus one hour due to time zone difference
-
-# df["played_at"] = pd.to_datetime(df['date_playing'] + ' ' + df['hour_playing'])
-
-# df["played_at"] = df["played_at"] + timedelta(hours=1, minutes=0) 
-
-
-# #transform date_playing into date format
-
-
-# #Filter on last 60 minute datas
-# deltaTime = datetime.today() - timedelta(hours=1, minutes=0)
-# df = df.loc[(df['played_at'] >= deltaTime)]
-
-# df = df.drop('played_at', axis=1)
-            
-
-##UPLOAD REFINE DATA ON S3
-#date_of_upload = datetime.now()
-# region_name=os.getenv("region_name")
-# aws_access_key_id=os.getenv("aws_access_key_id")
-# aws_secret_access_key=os.getenv("aws_secret_access_key")
-
-
-# s3_bucket = 'spotify-analytics'
-# s3_client = boto3.client("s3",
-#                   region_name=region_name,
-#                   aws_access_key_id=aws_access_key_id,
-#                   aws_secret_access_key=aws_secret_access_key)
-
-# with io.StringIO() as csv_buffer:
-#     df.to_csv(csv_buffer, index=False)
-
-#     response = s3_client.put_object(
-#         Bucket=s3_bucket, Key=f"data{date_of_upload}.csv", Body=csv_buffer.getvalue()
-#     )
-
-
-# azure_storage_account_key=os.getenv("azure_storage_account_key")
-# azure_storage_account_name=os.getenv("azure_storage_account_name")
-# azure_connection_string=os.getenv("azure_connection_string")
-
-
-# ## Create archive of previous data
-# # Source
-
-# def archiveToBlobStorage(source_container_name,source_file_path,target_container_name,target_file_path):
-#     blob_service_client = BlobServiceClient.from_connection_string(azure_connection_string)
-#     source_blob = (f"https://{azure_storage_account_name}.blob.core.windows.net/{source_container_name}/{source_file_path}")
-#     copied_blob = blob_service_client.get_blob_client(target_container_name, target_file_path)
-#     copied_blob.start_copy_from_url(source_blob)
-#     remove_blob = blob_service_client.get_blob_client(source_container_name, source_file_path)
-#     remove_blob.delete_blob()
-
-
-# last_data_upload = datetime.today()
-# archiveToBlobStorage("test1","data.csv","archive",f"data{last_data_upload}.csv")
-
-
-# ##UPLOAD REFINE DATA ON BLOB
-# data_output = io.StringIO()
-# data_output = df.to_csv (index=False, encoding = "utf-8",sep=';')
-
-
-# def uploadToBlobStorage(file_name,df,container_name):
-#    blob_service_client = BlobServiceClient.from_connection_string(azure_connection_string)
-#    blob_client = blob_service_client.get_blob_client(container=container_name, blob=file_name)
-#    blob_client.upload_blob(df,overwrite=True)
-
-
-# uploadToBlobStorage('data.csv',data_output,'test1')
